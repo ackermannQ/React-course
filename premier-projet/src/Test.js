6cf14127e8ca274e34d0d29542f89844a8ebefd8
ackermannQ <ackermann.quentin@gmail.com>
2020-09-01 19:27:10 +0200

    Using <Fragment>

---
+++ premier-projet/src/Test.js
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from 'react';
+import './Test.css';
+
+class Test extends Component {
+    render() {
+        return (
+            <Fragment>
+                <div className="Test">
+                    <h1>Very nice title</h1>
+                </div>
+                <h2>And a nice subtitle</h2>
+            </Fragment>
+        )
+    }
+}
+
+export default Test;
