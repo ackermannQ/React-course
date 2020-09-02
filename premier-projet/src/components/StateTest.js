fb8df83aa4fac92dff7570b976ddf914aff241dc
ackermannQ <ackermann.quentin@gmail.com>
2020-09-02 05:36:08 +0200

    handleClick() test

--- premier-projet/src/components/StateTest.js
+++ premier-projet/src/components/StateTest.js
@@ -21,25 +21,30 @@ const familyGang = {
 }
 
 class SateTest extends Component {
+    handleClick = () => console.log('Aging')
+
     state = {
         familyGang
     }
     render() {
-        const { family } = this.state;
-        return (
-            <Fragment>
-                <div className="Test">
-                    <h1>Testing class</h1>
-                </div>
-                <h2>Checkout the members of the fam'</h2>
-                <Membre name={familyGang.member1.name} surname="Ackermann"/>
-                <Membre name={familyGang.member2.name} surname="G"/>
-                <MembreChild name={familyGang.member3.name}>
-                  I'm so noot
-                </MembreChild>
-            </Fragment>
-        )
-    }
+      const { family } = this.state;
+      return (
+        <Fragment>
+          <div className="Test">
+              <h1>Testing class</h1>
+          </div>
+          <h2>Checkout the members of the fam'</h2>
+          <Membre name={familyGang.member1.name} surname="Ackermann"/>
+          <Membre name={familyGang.member2.name} surname="G"/>
+          <MembreChild name={familyGang.member3.name}>
+            I'm so noot
+          </MembreChild>
+          <button onClick={this.handleClick}>
+              Aging
+          </button>
+        </Fragment>
+      )
+  }
 }
 
 export default SateTest;
