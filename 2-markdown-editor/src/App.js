4ff84e8d98c82147bae003b0deceb07ae5c7e220
ackermannQ <ackermann.quentin@gmail.com>
2020-09-03 19:57:29 +0200

    Simple markdown editor Text saved locally with localStorage storing the state

--- 2-markdown-editor/src/App.js
+++ 2-markdown-editor/src/App.js
@@ -1,21 +1,57 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import { sampleText } from './sampleText'
+import marked from 'marked'
+
 
 class App extends Component {
+  state = {
+    text: sampleText
+  }
+
+  componentDidMount() {
+    const text = localStorage.getItem('text')
+    if (text) {
+      this.setState({ text })
+    } else {
+      this.setState({ text: sampleText })
+    }
+  }
+
+  componentDidUpdate() {
+    const { text } = this.state
+    localStorage.setItem('text', text)
+  }
+
+  handleChange = event => {
+    const text = event.target.value
+    this.setState({ text })
+  }
+
+  renderText = text => {
+    const __html = marked(text, { sanitize: true })
+    return { __html }
+  }
+
   render() {
     return (
       <div className="container">
+        <div className="row row-cols-1">
         <div className="row">
-        <div className="col-sm-6">
-            <textarea
-            className="font-controller"
-            rows="35">
-            </textarea>
+          <div className="markdown_container"
+          dangerouslySetInnerHTML={ this.renderText(this.state.text) }/>
           </div>
 
-          <div className="col-sm-6">
-            <h1>Markdown Editor</h1>
+        <div className="row">
+          <div className="markdown_editor">
+            <textarea onChange={this.handleChange}
+            className="font-controller"
+            rows="20"
+            cols="100"
+            value={this.state.text} />
           </div>
+        </div>
+
 
         </div>
       </div>
