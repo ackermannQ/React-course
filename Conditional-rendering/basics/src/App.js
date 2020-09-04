df79e6466ddb69d0f88eb90f705b2458c989323c
ackermannQ <ackermann.quentin@gmail.com>
2020-09-04 05:27:06 +0200

    Changing text with condition on onClick

--- Conditional-rendering/basics/src/App.js
+++ Conditional-rendering/basics/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App  extends Component {
+  state = {
+    text: "Hello",
+    isShown: true,
+    textToShow: "Hello,"
+  }
+
+  changeText = text => {
+    if (this.state.text === "Hello") {
+      this.setState({ text:"World!" })
+    } else {
+      this.setState({ text: "Hello" })
+    }
+  }
+
+  showText = () => {
+    if (this.state.isShown) {
+      this.setState({textToShow: "World!"})
+      this.setState({isShown: false})
+    } else {
+      this.setState({textToShow: "Hello"})
+      this.setState({isShown: true})
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <button onClick={this.changeText}>{this.state.text}</button>
+        <button onClick={this.showText}>CLICK HERE</button>
+        
+        <div>{this.state.textToShow}</div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
