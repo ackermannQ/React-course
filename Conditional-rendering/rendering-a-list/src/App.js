f993162aa079fdf09875d2181125a7386d3f17fa
ackermannQ <ackermann.quentin@gmail.com>
2020-09-06 20:58:32 +0200

    rendering of a list

---
+++ Conditional-rendering/rendering-a-list/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const family = {
+  membre1: {
+    name: "Quentin",
+    age: 25
+    },
+  membre2: {
+    name: "Laura",
+    age: 24
+  },
+  membre3: {
+    name: "Onny",
+    age: 2
+  }
+}
+
+class App extends Component {
+  state = {
+    // family
+  }
+  render() {
+    const list = Object.keys(family)
+    .map(membre => {
+      console.log(family[membre])
+    })
+    return (
+      <div className="container">
+      <h1>Family Gaaaaaaaaaang</h1>
+      </div>
+    )
+  }
+}
+
+export default App
