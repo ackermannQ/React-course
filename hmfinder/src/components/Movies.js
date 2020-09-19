e8ade0058127b5a7cfb535af036139b1816c9e72
ackermannQ <ackermann.quentin@gmail.com>
2020-09-19 20:09:28 +0200

    Get horror movies

--- hmfinder/src/components/Movies.js
+++ hmfinder/src/components/Movies.js
@@ -7,11 +7,14 @@ class Movies extends Component {
   constructor(props) {
     super(props)
     this.state = { rows: null }
+  }
+  
+  componentDidMount() {
     this.performSearch()
   }
 
   performSearch = () => {
-    const urlString = "https://api.themoviedb.org/3/search/movie?api_key=47e60775dec848a0846f06525377d027&query=Horror&include_adult=true"
+    const urlString = "https://api.themoviedb.org/3/search/movie?api_key=47e60775dec848a0846f06525377d027&query=`${ inputText }`&genre=27&&sort_by=vote_average.desc&vote_count.gte=10&&primary_release_year=2015&include_adult=true"
     $.ajax({
       url: urlString,
       success: (searchResults) => {
@@ -20,7 +23,8 @@ class Movies extends Component {
 
         var movieRows = []
         results.forEach((movie) => {
-          const movieRow = <MovieRow movie={ movie } />
+          movie.posterSrc = "https://image.tmdb.org/t/p/w220_and_h330_face/" + movie.poster_path
+          const movieRow = <MovieRow key={movie.id} movie={ movie } />
           movieRows.push(movieRow)
         })
 
